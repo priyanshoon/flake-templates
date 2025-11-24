a3b1c3a74d7134585164bb5953469d42d12e09e3
Priyanshu Gupta <priyanshoon.pg@gmail.com>
2025-11-25 03:14:49 +0530

    flake.nix

--- flake.nix
+++ flake.nix
@@ -1,22 +1,12 @@
 {
   description = "personal nix flake template for devenv";
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-parts.url = "github:hercules-ci/flake-parts";
-  };
-  outputs = inputs@{ self, flake-parts, ... } :
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-      ];
-
-      perSystem = { pkgs, ... }:
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            nil
-            nixd
-          ];
+  outputs =
+    { self }:
+    {
+      templates = rec {
+        c = {
+          path = ./c;
+          description = "C project environment";
         };
       };
     };
